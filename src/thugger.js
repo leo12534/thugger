0593fa09aed977a410e64cf9a4fb184b7ccafb49
Leandro Bermeo <leandrofbermeo@gmail.com>
2021-06-25 15:32:28 +0100

    Moving files into a src folder

--- src/thugger.js
+++ src/thugger.js
@@ -1,21 +1,5 @@
 const keys = document.querySelectorAll('.key');
 
-function playAdlib(audio) {
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-}
-
-function addRemoveBorder(key) {
-    key.classList.add('playing');
-    keys.forEach((key) =>
-        key.addEventListener('transitionend', function (e) {
-            if (e.propertyName !== 'transform') return;
-            key.classList.remove('playing');
-        })
-    );
-}
-
 function playAudio(e) {
     let keyCode; // Variable to store keyCode, depending on click or keydown
     if (e.type === 'click') {
@@ -30,6 +14,23 @@ function playAudio(e) {
     if (!currentKey) return;
     playAdlib(audio);
     addRemoveBorder(currentKey);
+}
+
+function playAdlib(audio) {
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function addRemoveBorder(key) {
+    key.classList.add('playing');
+    keys.forEach((key) =>
+        key.addEventListener('transitionend', function (e) {
+            if (e.propertyName !== 'transform') return;
+            key.classList.remove('playing');
+        })
+    );
+    console.log(key);
 }
 
 window.addEventListener('keydown', playAudio);
